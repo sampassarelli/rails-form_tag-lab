1891eac0cd1123922278c150eb3004ab5e31d5de
sampassarelli <passarelli.sam@gmail.com>
2020-11-21 20:59:48 -0600

    commit tree b4d9a6ba905e5fcc97a78fe7b9bdec64b2ad2218

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,8 +4,18 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def new
+  end
+
   def show
     @student = Student.find(params[:id])
   end
+
+  def create 
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to new_student_path
+  end
+
+  
 
 end
